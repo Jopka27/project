dbd8568338c952445fb48e1c99daa18e5210e8e6
Serg <gromutsergej03@gmail.com>
2023-02-04 17:13:36 +0100

    first commit

---
+++ js/sccript.js
@@ -0,0 +1,12 @@
+"use sctrict"
+const arrOfAnswer = [];
+ 
+ arrOfAnswer[0] = prompt("Are you her?");
+
+ arrOfAnswer[1] = prompt("How old are you?","");
+
+ arrOfAnswer[2] = prompt("Enter your Sex", "");
+
+
+
+console.log(typeof(arrOfAnswer)); 
