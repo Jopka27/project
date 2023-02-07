e2ba055e9cf0f8cf7cfe425cc70b3045b0494181
Serg <gromutsergej03@gmail.com>
2023-02-07 17:42:03 +0100

    job with Conditions

--- js/sccript.js
+++ js/sccript.js
@@ -1,22 +1,20 @@
 "use strict"
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
+  num = 50;
 
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: {},
-    privat: false
-};
-
-const a = prompt("Один из последних просмотренных фильмов?",''),
-      b = prompt("На сколько оцените его?",''),
-      c = prompt("Один из последних просмотренных фильмов?",''),
-      d = prompt("На сколько оцените его?",'');
-
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
-
-      console.log(personalMovieDB);
+  (num === 50) ? console.log('OK!') : console.log('ERROR');
+  
+  switch(num){
+        case 49:
+            console.log("Error");
+            break;
+        case 100:   
+            console.log("Error");
+            break; 
+        case 50:   
+            console.log("OK!");
+            break; 
+        default:
+            console.log("Not in this time!");
+            break; 
+  }
